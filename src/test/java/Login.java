243c4aa5890da2e54d2f508d8e80b75e3dc6ab37
aiswaryavin <aiswaryakrishnan100@gmail.com>
2025-02-11 15:54:01 +0400

    chnged

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -8,7 +8,7 @@ public class Login extends Main {
     @Test(priority=1)
     public void login(){
         driver.get("http://192.168.1.193:3000");
-    clicked(By.xpath("//*[@id=\"main\"]/div/div/div[2]/a/button"));
+        clicked(By.xpath("//*[@id=\"main\"]/div/div/div[2]/a/button"));
         enterText(By.xpath("//input[@id=':r0:']"), "superadmin");
         enterText(By.name("password"), "123456");
         clicked(By.xpath("/html/body/div/div[2]/form/button"));
