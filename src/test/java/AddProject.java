065fd0afc13b825d06a44bd4197980d56f081c50
aiswaryavin <aiswaryakrishnan100@gmail.com>
2025-02-20 11:11:33 +0400

    addproject section changes

--- src/test/java/AddProject.java
+++ src/test/java/AddProject.java
@@ -1,43 +1,88 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class AddProject extends Main {
-    private By projectstab= By.xpath("//span[normalize-space()='Projects']");
-    private By addproject= By.xpath("//span[normalize-space()='Add Project']");
+    private By projectsTab = By.xpath("//span[normalize-space()='Projects']");
+    private By addProject = By.xpath("//span[normalize-space()='Add Project']");
     private By projectName = By.name("project_name");
     private By licenseNumber = By.name("license_no");
     private By projectNumber = By.name("project_no");
     private By startingPrice = By.name("starting_price");
+    private By DeveloperCompany = By.xpath("//input[@placeholder='Developer Company']");
+   // private By firstOption = By.xpath("//li[contains(@class, 'MuiAutocomplete-option') and normalize-space(text())='OMNIYAT']");
+    private By country= By.xpath("//*[@placeholder='Select Country']");
+    private By state= By.xpath("//*[@placeholder='Select State']");
+    private By city= By.xpath("//*[@placeholder='Select City']");
+    private By community= By.xpath("//*[@placeholder='Select Community']");
+    private By subcommunity= By.xpath("//*[@placeholder='Select Sub Community']");
 
     public AddProject(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
-    public void projecttab()
-    {
-        driver.findElement(projectstab).click();
 
+    // Generic method to wait for an element to be clickable and then click it
+    public void clickWhenReady(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
-    public void addproject(){
-        driver.findElement(addproject).click();
+
+    // Generic method to wait for an element to be visible and then send keys
+    private void enterTextWhenReady(By locator, String text) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(text);
+    }
+
+    public void openProjectsTab() {
+        clickWhenReady(projectsTab);
+    }
+
+    public void clickAddProject() {
+        clickWhenReady(addProject);
     }
 
     public void enterProjectName(String name) {
-        driver.findElement(projectName).sendKeys(name);
+        enterTextWhenReady(projectName, name);
     }
 
     public void enterLicenseNumber(String license) {
-        driver.findElement(licenseNumber).sendKeys(license);
+        enterTextWhenReady(licenseNumber, license);
     }
 
     public void enterProjectNumber(String number) {
-        driver.findElement(projectNumber).sendKeys(number);
+        enterTextWhenReady(projectNumber, number);
     }
 
     public void enterStartingPrice(String price) {
-        driver.findElement(startingPrice).sendKeys(price);
+        enterTextWhenReady(startingPrice, price);
+    }
+    public void devopercompany(String devop){
+        enterTextWhenReady(DeveloperCompany,devop);
+     //   wait.until(ExpectedConditions.presenceOfElementLocated(firstOption));
+
+        // Click the first option
+     //   clickElement(firstOption);
+
+    }
+    public void clickElement(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).click();
+    }
+    public void selectcountry(String cou){
+        enterTextWhenReady(country,cou);
+    }
+    public void selectstate(String st){
+        enterTextWhenReady(state,st);
+    }
+    public void selectcity(String ct){
+        enterTextWhenReady(city,ct);
+    }
+    public void selectcommunity(String comm){
+        enterTextWhenReady(community,comm);
+    }
+    public void selectsubcommunity(String subcom){
+        enterTextWhenReady(subcommunity,subcom);
     }
 }
--- src/test/java/AddProjectTest.java
+++ src/test/java/AddProjectTest.java
@@ -7,11 +7,20 @@ public class AddProjectTest extends Main {
     public void testAddProject() throws InterruptedException {
         addProject = new AddProject(driver);  // Initialize it here
         Thread.sleep(1000);
-        addProject.projecttab();
-        addProject.addproject();
+        addProject.openProjectsTab();
+        addProject.clickAddProject();
         addProject.enterProjectName("Sky Towers");
         addProject.enterLicenseNumber("12345");
         addProject.enterProjectNumber("PR-5678");
         addProject.enterStartingPrice("500000");
+        //Thread.sleep(1000);
+
+        addProject.devopercompany("OMNIYAT");
+        addProject.selectcountry("United Arab Emirates");
+        addProject.selectstate("Abu Dhabi");
+        addProject.selectcity("Abu Dhabi");
+        addProject.selectcommunity("Abu Dhabi Gate City");
+        addProject.selectsubcommunity("Seashore");
+
     }
 }
